2290bbd3b7435dfc5fae04a648dcd61a23eeab33
Marine Bruno <marinebruno@Marines-Mac.local>
2021-06-01 16:47:31 -0500

    Changes to routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,74 +1,33 @@
-// Dependencies
+//// Dependency - fs
 const fs = require("fs");
-
-// imported 'uuid' npm package for unique id
 const { v4: uuidv4 } = require('uuid');
 
-// ROUTING
+//// API Routes
 module.exports = function (app) {
-
-    // API GET Request
+    fs.readFile("db/db.json","utf8", (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+    ////GET
     app.get("/api/notes", (request, response) => {
-        
-        console.log("\n\nExecuting GET notes request");
-
-        // Read 'db.json' file 
-        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        
-        console.log("\nGET request - Returning notes data: " + JSON.stringify(data));
-        
-        // Send read data to response of 'GET' request
-        response.json(data);
+        console.log("Returning note data!");
+        response.json(notes);
     });
-
-
-    // API POST Request
+    ////POST
     app.post("/api/notes", (request, response) => {
-
-        // Extracted new note from request body.  
         const newNote = request.body;
-        
-        console.log("\n\nPOST request - New Note : " + JSON.stringify(newNote));
-
-        // Assigned unique id obtained from 'uuid' package
         newNote.id = uuidv4();
-
-        // Read data from 'db.json' file
-        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    
-        // Pushed new note in notes file 'db.json'
-        data.push(newNote);
-
-        // Written notes data to 'db.json' file
-        fs.writeFileSync('./db/db.json', JSON.stringify(data));
-        
-        console.log("\nSuccessfully added new note to 'db.json' file!");
-
-        // Send response
-        response.json(data);
+        notes.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+        console.log("Added new note!");
+        response.json(notes);
     });
-
-
-    // API DELETE request
+    ////DELETE
     app.delete("/api/notes/:id", (request, response) => {
-
-        // Fetched id to delete
         let noteId = request.params.id.toString();
-        
-        console.log(`\n\nDELETE note request for noteId: ${noteId}`);
-
-        // Read data from 'db.json' file
-        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-
-        // filter data to get notes except the one to delete
-        const newData = data.filter( note => note.id.toString() !== noteId );
-
-        // Write new data to 'db.json' file
+        const newData = notes.filter( note => note.id.toString() !== noteId );
         fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-        
-        console.log(`\nSuccessfully deleted note with id : ${noteId}`);
-
-        // Send response
+        console.log("Deleted note!");
         response.json(newData);
+    });
     });
 };
--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-// Dependencies
+//// Dependencies, app, and port
 const express = require("express");
-
-// Express configuration
-//Tells node that we are creating an 'express' server
 const app = express();
-
-// Sets an initial port.
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
 
-// Sets up the Express app to handle data parsing
+//// Data Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// ROUTES
+//// Routing
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
-
-app.listen(PORT, function() {
-    console.log(`App listening on PORT: ${PORT}`);
-});
